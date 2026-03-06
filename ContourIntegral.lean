ed37a51e2f677c7010c813c69a973f00a0ee8bc2
sharky564 <sharkesa@gmail.com>
2026-03-07 03:33:59 +1100

    Added integral 1/((x^2+1)^2) from -infinity to infinity for higher-order poles

--- ContourIntegral.lean
+++ ContourIntegral.lean
@@ -2,3 +2,4 @@ import ContourIntegral.RectIntegral
 import ContourIntegral.ResidueTheorem
 import ContourIntegral.CosDivSqAddOne
 import ContourIntegral.InvProdSqAddOneSqAddFour
+import ContourIntegral.InvSqAddOneSq
